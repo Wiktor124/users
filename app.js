c7d93c451a2b5e2db5900e1eb9f04a3523cf0615
Wiktor <victortkd88@gmail.com>
2022-08-26 18:24:10 -0600

    new properties

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.json())
 app.use(express.static('public'))
 mongoose.connect('mongodb+srv://victor:db123456@cluster0.h3p4lnt.mongodb.net/miapp?retryWrites=true&w=majority')
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/index.html`));
+})
+
 app.get('/users', user.list)
 app.post('/users', user.create)
 app.get('/users/:id', user.get)
@@ -15,9 +19,6 @@ app.put('/users/:id', user.update)
 app.patch('/users/:id', user.update)
 app.delete('/users/:id', user.destroy)
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/index.html`));
-})
 
 app.get('*', (req, res) => {
   res.status(404).send('Esta pagina no existe')
