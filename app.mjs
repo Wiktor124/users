ac2ae6789cbbf3d4dcb8f390af49ede951bef699
Wiktor <victortkd88@gmail.com>
2022-09-04 10:05:27 -0600

    DB_URI agregado

--- app.mjs
+++ app.mjs
@@ -6,12 +6,11 @@ import { list } from './api/controllers/user.controller.js';
 
 const app = express()
 
-
 const PORT = process.env.PORT;
 app.use(express.json())
 app.use(express.static('public'))
 
-mongoose.connect(process.env.DB_URI)
+mongoose.connect(`mongodb+srv://victor:${process.env.DB_URI}@cluster0.h3p4lnt.mongodb.net/?retryWrites=true&w=majority`)
 
 app.use('/users', appRouter, list)
 
